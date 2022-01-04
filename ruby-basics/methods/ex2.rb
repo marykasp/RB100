240764bb65026f01027aba896664365bbb2922f1
Mary Kasparian <kasparian.mary@gmail.com>
2022-01-04 11:38:09 -0600

    finish method exercises

---
+++ ruby-basics/methods/ex2.rb
@@ -0,0 +1,8 @@
+# Write a method named print_me so that I'm printing the return value! is printed when running the following code:
+
+def print_me
+  # return line from method
+  "I'm printing the return value!"
+end
+
+puts print_me
---
+++ ruby-basics/methods/ex5.rb
@@ -0,0 +1,9 @@
+# Using the following code, write a method called car that takes two arguments and prints a string containing the values of both arguments.
+
+
+def car(value1, value2)
+  # puts value1 + " " + value2
+  puts "#{value1} #{value2}"
+end
+
+car('Toyota', 'Corolla')
